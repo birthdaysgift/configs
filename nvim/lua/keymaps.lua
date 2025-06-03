03beb5e35e51a2677fd115162c2d82bf6d6a2959
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2025-06-03 15:20:49 +0400

    [nvim] Disable <BS> in favor of <C-H>

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,9 @@
 -- list actually set keymaps
 -- :map
 
+-- disable backspace (it's convenient to use <C-H> instead)
+vim.keymap.set("i", "<BS>", "<Nop>", { remap = false })
+
 -- disable search highlights
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -20,8 +20,11 @@ return {
       -- break_undo = true, -- switch for basic rule break undo sequence
       -- check_ts = false,
       -- map_cr = true,
-      -- map_bs = true, -- map the <BS> key
-      -- map_c_h = false, -- Map the <C-h> key to delete a pair
+
+      -- I use <C-H> instead of <BS>
+      map_bs = false, -- map the <BS> key
+      map_c_h = true, -- Map the <C-h> key to delete a pair
+
       -- map_c_w = false, -- map <c-w> to delete a pair if possible
     },
   },
