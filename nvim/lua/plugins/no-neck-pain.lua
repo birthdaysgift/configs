d18f5d73574c62eb2f143eff7803d412681b78f4
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2025-06-13 15:36:38 +0400

    [nvim] Add no-neck-pain
    it really feels bad to work with large monitor and always look at the
    left side of it, so let's center nvim buffers

---
+++ nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+    config = function()
+      require("no-neck-pain").setup({width=130})
+
+      vim.keymap.set("n", "<leader>N", ":NoNeckPain<CR>", { desc = "Toggle [N]oNeckPain" })
+
+      vim.api.nvim_create_autocmd(
+        {
+          "BufEnter",
+        }, {
+          callback = function()
+            require("no-neck-pain").enable()
+          end,
+        }
+      )
+    end,
+  }
+}
