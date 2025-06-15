3f044bc428e2d663f74fb8caf5f123f82f766095
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2025-06-15 15:41:26 +0400

    [nvim] Little refactoring for no-neck-pain config

--- nvim/lua/plugins/no-neck-pain.lua
+++ nvim/lua/plugins/no-neck-pain.lua
@@ -7,15 +7,7 @@ return {
 
       vim.keymap.set("n", "<leader>N", ":NoNeckPain<CR>", { desc = "Toggle [N]oNeckPain" })
 
-      vim.api.nvim_create_autocmd(
-        {
-          "BufEnter",
-        }, {
-          callback = function()
-            require("no-neck-pain").enable()
-          end,
-        }
-      )
+      vim.api.nvim_create_autocmd({ "BufEnter" }, { callback = require("no-neck-pain").enable })
     end,
   }
 }
