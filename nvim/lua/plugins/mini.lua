251d9a9be3bed3aa3fc9130ff4324cdc1306b9a0
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2025-07-02 18:34:07 +0400

    [nvim] Add "mini.splitjoin"

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -13,7 +13,21 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       require("mini.ai").setup({ n_lines = 500 })
+
       require("mini.surround").setup()
+
+      require("mini.splitjoin").setup({
+        split = { 
+          hooks_post = { 
+            require("mini.splitjoin").gen_hook.add_trailing_separator(),
+          },
+        },
+        join  = {
+          hooks_post = {
+            require("mini.splitjoin").gen_hook.del_trailing_separator(),
+          }
+        },
+      })
     end,
   },
 }
