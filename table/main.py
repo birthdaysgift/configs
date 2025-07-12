9667ca84f6008f2d4834d82342dbc6328b8d6880
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2025-07-12 17:04:20 +0400

    [bash] Add table -> csv conversion for table/main.py script

--- table/main.py
+++ table/main.py
@@ -1,23 +1,68 @@
 import argparse
+import csv
 import fileinput
+import io
+import itertools
 
 import prettytable
 
 
 def main():
+    args = parse_args()
+    text = read_input()
+
+    if text.startswith("+") or args.csv:
+        to_csv(text, args.separator)
+        return
+
+    print(to_table(text, args.separator))
+
+
+def to_table(text, separator):
+    with io.StringIO(text) as f:
+        reader = csv.reader(f, delimiter=separator)
+        rows = list(reader)
+
+    table = prettytable.PrettyTable(field_names=rows[0])
+    table.add_rows(rows[1:])
+    return table
+
+
+def to_csv(text, separator):
+    rows = text.split("\n")
+
+    header = rows[:3]
+    header_border = header[0]
+    plus_positions = [index for (index, char) in enumerate(header_border) if char == "+"]
+
+    cleaned_data = []
+
+    cell_positions = itertools.pairwise(plus_positions)
+    for row in [header[1]] + rows[3:-2]:
+        cleaned_row = []
+        for start, end in cell_positions:
+            cleaned_row.append(row[start+1:end].strip(" "))
+        cleaned_data.append(cleaned_row)
+
+    with io.StringIO() as f:
+        writer = csv.writer(f, delimiter=separator, lineterminator="\n")
+        for row in cleaned_data:
+            writer.writerow(row)
+        print(f.getvalue().rstrip("\n"))
+
+
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--separator", default=",")
-    args = parser.parse_args()
+    parser.add_argument("--csv", action="store_true")
+    return parser.parse_args()
 
+
+def read_input():
     lines = []
     for line in fileinput.input(files=[]):
-        lines.append(line.strip("\n").split(args.separator))
-
-    header = lines[0]
-    table = prettytable.PrettyTable(field_names=header)
-    table.add_rows(lines[1:])
-
-    print(table)
+        lines.append(line)
+    return "".join(lines)
 
 
 if __name__ == "__main__":
