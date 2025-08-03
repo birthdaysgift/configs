56dda7a40e9d0e74486b50a00aecc490bf7a3f89
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2025-08-03 11:30:50 +0400

    [nvim] Add url-open plugin

---
+++ nvim/lua/plugins/url-open.lua
@@ -0,0 +1,17 @@
+return {
+
+  {
+      "sontungexpt/url-open",
+      event = "VeryLazy",
+      cmd = "URLOpenUnderCursor",
+      config = function()
+          local status_ok, url_open = pcall(require, "url-open")
+          if not status_ok then
+              return
+          end
+          url_open.setup({})
+          vim.keymap.set("n", "<leader>o", ":URLOpenUnderCursor<CR>", { desc = "[O]pen URL" })
+      end,
+  },
+
+}
