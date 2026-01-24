a0c03c2ef633f4ff8de260227e1cc2acfd91a809
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2026-01-24 15:56:05 +0400

    [nvim] Add toggling to diffview plugin

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -4,17 +4,22 @@ return {
     config = function()
       require("diffview").setup({
         keymaps = {
-          disable_defaults = true
+          disable_defaults = false
         }
       })
 
-      vim.keymap.set(
-        "n",
-        "<leader>v",
-        function()
-          vim.cmd("DiffviewOpen")
-        end
-      )
+      local diffview_opened = false
+      local function diffview_toggle()
+          if not diffview_opened then
+            vim.cmd("DiffviewOpen")
+            diffview_opened = true
+          else
+            vim.cmd("DiffviewClose")
+            diffview_opened = false
+          end
+      end
+
+      vim.keymap.set("n", "<leader>v", diffview_toggle, { desc = "Diff [V]iew Toggle"})
     end
   }
 }
