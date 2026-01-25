f028297e55fef15604ae2a8816b7c43443378371
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2026-01-25 10:59:46 +0400

    [nvim] Remove dap-helper
    since it's been broken for some reason
    
    need to check (and cleanup) its local files in case I will have to
    install it again at some time

--- nvim/lua/plugins/dap-helper.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'daic0r/dap-helper.nvim',
-    dependencies = { "rcarriga/nvim-dap-ui", "mfussenegger/nvim-dap" },
-    config = function()
-      require("dap-helper").setup()
-    end
-  },
-}
