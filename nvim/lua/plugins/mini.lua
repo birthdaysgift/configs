81185cf2f959ef9519b67fe6da8fc4e1fc674bf2
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2026-01-25 11:05:06 +0400

    [nvim] Map comments on gj and add comment object
    so now I can do gjgj to uncomment whole block

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,6 +5,22 @@ return {
       options = {
         ignore_blank_line = true,
       },
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        -- Toggle comment (like `gcip` - comment inner paragraph) for both
+        -- Normal and Visual modes
+        comment = 'gj',
+
+        -- Toggle comment on current line
+        comment_line = 'gjj',
+
+        -- Toggle comment on visual selection
+        comment_visual = 'gj',
+
+        -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+        -- Works also in Visual mode if mapping differs from `comment_visual`
+        textobject = 'gj',
+      },
     },
   },
 
