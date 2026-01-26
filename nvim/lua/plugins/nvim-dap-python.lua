5cd07d44fc533ee34d4ec75cd136d0d3aca924a9
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2026-01-26 11:49:32 +0400

    [nvim] Add more python-dap configurations

--- nvim/lua/plugins/nvim-dap-python.lua
+++ nvim/lua/plugins/nvim-dap-python.lua
@@ -27,6 +27,58 @@ return {
         cwd = '${workspaceFolder}/api',
         console = 'integratedTerminal',
       })
+      table.insert(require('dap').configurations.python, {
+        type = 'python',
+        request = 'launch',
+        name = '(ipapi) api/images/main.py',
+        module = 'images.main',
+        cwd = '${workspaceFolder}/api',
+        console = 'integratedTerminal',
+      })
+      table.insert(require('dap').configurations.python, {
+        type = 'python',
+        request = 'launch',
+        name = '(ipapi) lookup/src/app.py',
+        module = 'src.app',
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+      })
+      table.insert(require('dap').configurations.python, {
+        type = 'python',
+        request = 'launch',
+        name = '(ipapi) lookup/src/feeder.py feed',
+        module = 'src.feeder',
+        args = {"feed"},
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+      })
+      table.insert(require('dap').configurations.python, {
+        type = 'python',
+        request = 'launch',
+        name = '<ass-service>/src/app.py',
+        module = 'src.app',
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+        justMyCode = false,
+      })
+      table.insert(require('dap').configurations.python, {
+        type = 'python',
+        request = 'launch',
+        name = '<nvgraph>/src/main.py',
+        module = 'src.main',
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+        justMyCode = false,
+      })
+      table.insert(require('dap').configurations.python, {
+        type = 'python',
+        request = 'launch',
+        name = '<auth-api>/src/main.py',
+        module = 'app.main',
+        cwd = '${workspaceFolder}/backend/app',
+        console = 'integratedTerminal',
+        justMyCode = false,
+      })
 
       vim.keymap.set("n", "<leader>dp", require("dap-python").test_method, { desc = "[P]ytest method under cursor" })
 
